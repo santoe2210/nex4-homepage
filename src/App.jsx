f273a862e62736d92b319462c5396d9fe208e43a
ArntBhoneKo <59755613+ArntBhoneKo@users.noreply.github.com>
2025-06-04 15:07:03 +0900

    draft  done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import Header from './components/Header';
 import Hero from './components/HeroSection';
-import './App.css';
+// import ProblemSection from './components/ProblemSection';
+// import SolutionSection from './components/SolutionSection';
+// import ServicesSection from './components/ServicesSection';
+// import Logos from './components/Logos';
+// import Features from './components/Features';
+// import CTA from './components/CTA';
+// import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      {/* <ProblemSection />
+      <SolutionSection />
+      <ServicesSection />
+      <Logos />
+      <Features />
+      <CTA />
+      <Footer /> */}
     </>
   );
 }
